35844e09eceda3aff045636bfc8d1358d69f7143
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-07-03 19:21:04 +0530

    Day 12 String Reverse Each Word in a Sentence

---
+++ src/day12_Arrays_Strings_DSA_Part2/ReverseEachWord_2.java
@@ -0,0 +1,20 @@
+package day12_Arrays_Strings_DSA_Part2;
+
+
+// Write a Java program to reverse each word in a string while keeping their position in the sentence.
+
+public class ReverseEachWord_2 {
+    public static void main(String[] args) {
+        String sentence = "Janu Love You";
+        String[] words = sentence.split(" ");
+        StringBuilder reversedSentence = new StringBuilder();
+
+        for (String word : words) {
+            StringBuilder sb = new StringBuilder(word);
+            reversedSentence.append(sb.reverse()).append(" ");
+        }
+
+        System.out.println("Original Sentence: " + sentence);
+        System.out.println("Reversed Worlds: " + reversedSentence.toString().trim());
+    }
+}
