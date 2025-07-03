47517cc7112d09b76eb741f9f89c4fd9dbf08a80
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-07-03 19:46:14 +0530

    Day 12 Write a program to count how many times each character appears in a string.

---
+++ src/day12_Arrays_Strings_DSA_Part2/CharacterFrequency_2.java
@@ -0,0 +1,23 @@
+package day12_Arrays_Strings_DSA_Part2;
+
+// Write a program to count how many times each character appears in a string.
+
+import java.util.HashMap;
+
+public class CharacterFrequency_2 {
+    public static void main(String[] args) {
+        String input = "axemsrzy";
+
+        HashMap<Character, Integer> freqMap = new HashMap<>();
+
+        for (char ch : input.toCharArray()) {
+            if (ch == ' ') continue;    // skip space
+
+            freqMap.put(ch, freqMap.getOrDefault(ch, 0) + 1) ;
+        }
+
+        for (char key : freqMap.keySet()) {
+            System.out.println(key + " -> " + freqMap.get(key) + " time(s)");
+        }
+    }
+}
