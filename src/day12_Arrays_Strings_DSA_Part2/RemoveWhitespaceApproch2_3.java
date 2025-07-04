38811cbd81917a0211fea85adb9a21552b92e543
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-07-04 15:07:19 +0530

    Day 12 Remove All Whitespaces from a String,  Using Approach 2: Using a for loop (Manual way)

---
+++ src/day12_Arrays_Strings_DSA_Part2/RemoveWhitespaceApproch2_3.java
@@ -0,0 +1,16 @@
+package day12_Arrays_Strings_DSA_Part2;
+
+public class RemoveWhitespaceApproch2_3 {
+    public static void main(String[] args) {
+        String str = "Approch 2 using for loop";
+        String result = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) != ' ') {
+                result += str.charAt(i);
+            }
+        }
+        System.out.println("Before Spaces: " + str);
+        System.out.println("After spaces: " + result);
+    }
+}
