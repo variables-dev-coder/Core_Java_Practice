e2c9504fc66e76fd1b70533e388564a76f6aaaad
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-07-05 16:03:43 +0530

    Day 12 Remove Consecutive Duplicates

---
+++ src/day12_Arrays_Strings_DSA_Part2/RemoveAdjacentDuplicates.java
@@ -0,0 +1,28 @@
+package day12_Arrays_Strings_DSA_Part2;
+
+import java.util.Stack;
+
+public class RemoveAdjacentDuplicates {
+
+    public static String removeDuplicates(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (!stack.isEmpty() && stack.peek() == ch) {
+                stack.pop();
+            } else {
+                stack.push(ch);
+            }
+        }
+
+        // Reconstruct string
+        StringBuilder result = new StringBuilder();
+        for (char ch : stack) {
+            result.append(ch);
+        }
+        return result.toString();
+    }
+    public static void main(String[] args) {
+        System.out.println(removeDuplicates("abbaca"));
+    }
+}
