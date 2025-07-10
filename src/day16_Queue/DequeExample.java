99780dedf11961a96544d23039ad2ba1e7a14fde
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-07-10 20:08:04 +0530

    Day 16 Queue programs

---
+++ src/day16_Queue/DequeExample.java
@@ -0,0 +1,25 @@
+package day16_Queue;
+
+// Java Deque (Double-Ended Queue)
+// You can insert and delete from both ends.
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class DequeExample {
+    public static void main(String[] args) {
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        deque.addFirst(10);    // [10]
+        deque.addLast(20);     // [10, 20]
+        deque.addFirst(30);    // [30, 10, 20]
+
+        System.out.println(deque);
+
+        deque.removeLast();
+        System.out.println(deque);
+
+        deque.removeFirst();
+        System.out.println(deque);
+    }
+}
