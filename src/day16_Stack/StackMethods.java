f6c7990332d344823f80b80a74b206d1d9b8ea98
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-07-10 16:49:23 +0530

    Day 15 Java Stack example

---
+++ src/day16_Stack/StackMethods.java
@@ -0,0 +1,31 @@
+package day16_Stack;
+
+// Stack All Methods
+
+import java.util.Stack;
+
+public class StackMethods {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+
+        //Push
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+
+        // peek
+        System.out.println("Top: " + stack.peek());
+
+        // Pop
+        System.out.println("Popped: " + stack.pop());
+
+        // Size
+        System.out.println("Size: " + stack.size());
+
+        // Search
+        System.out.println("Position of 10: " + stack.search(10));
+
+        // Empty check
+        System.out.println("Is empty: " + stack.isEmpty());
+    }
+}
