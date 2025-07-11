654d855f2a84e5e3aef0e8333f72c047e3317230
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-07-11 22:29:11 +0530

    Day 16 Some Priority Queue Programms

---
+++ src/day16_PriorityQueue_Heap/SlidingWindowMax.java
@@ -0,0 +1,36 @@
+package day16_PriorityQueue_Heap;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class SlidingWindowMax {
+
+    public static int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        int[] result = new int[n - k + 1];
+
+        // Max - Heap with (value, index)
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> b[0] - a[0]);
+
+        for (int i = 0; i < n; i++) {
+            // Add current element with index
+            maxHeap.offer(new int[] {nums[i], i});
+
+            // Remove elements outside the window
+            while (maxHeap.peek()[1] <= i - k)
+                maxHeap.poll();
+
+            // Record max from heap
+            if (i >= k -1)
+                result[i - k + 1] = maxHeap.peek()[0];
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 3, -1, -3, 5, 3, 6, 7};
+        int k = 3;
+        System.out.println(Arrays.toString(maxSlidingWindow(nums, k)));
+
+    }
+}
--- src/day16_PriorityQueue_Heap/TopKFrequentElements.java
+++ src/day16_PriorityQueue_Heap/TopKFrequentElements.java
@@ -2,31 +2,35 @@ package day16_PriorityQueue_Heap;
 
 import java.util.*;
 
-public class TopKFrequent {
+public class TopKFrequentElements {
 
     public static List<Integer> topKFrequent(int[] nums, int k) {
+        // Step 1: Count frequency
         Map<Integer, Integer> freqMap = new HashMap<>();
         for (int num : nums)
             freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
 
-        // Min-Heap of (frequency, number)
-        PriorityQueue<Map.Entry<Integer, Integer>> pq =
+        // Step 2 : Min - Heap based on frequency
+        PriorityQueue<Map.Entry<Integer, Integer>> minHeap =
                 new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
 
         for (Map.Entry<Integer, Integer> entry : freqMap.entrySet()) {
-            pq.offer(entry);
-            if (pq.size() > k) pq.poll(); // remove lowest freq
+            minHeap.offer(entry);
+            if (minHeap.size() > k)
+                minHeap.poll();      // remove least frequent
         }
 
+        // Step 3 : Extract result
         List<Integer> result = new ArrayList<>();
-        for (Map.Entry<Integer, Integer> entry : pq)
+        for (Map.Entry<Integer, Integer> entry : minHeap)
             result.add(entry.getKey());
 
         return result;
     }
 
     public static void main(String[] args) {
-        int[] nums = {1,1,1,2,2,3};
-        System.out.println(topKFrequent(nums, 2));
+        int[] nums = {1, 1, 1, 2, 2, 3};
+        int k = 2;
+        System.out.println(topKFrequent(nums, k));
     }
 }
