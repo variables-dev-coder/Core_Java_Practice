b42233afeb9aacc89be9b3a7e749e31222ba592c
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-07-28 10:54:14 +0530

    Day 29, Circular Doubly Linked List insert at end example

---
+++ src/day29_Circular_Doubly_Linked_List/CircularDoublyLinkedList.java
@@ -0,0 +1,83 @@
+package day29_Circular_Doubly_Linked_List;
+
+
+// Circular Doubly Linked List, Insert at End (Tail)
+
+public class CircularDoublyLinkedList {
+    Node1 head = null;
+    Node1 tail = null;
+
+    class Node1 {
+        int data;
+        Node1 next;
+        Node1 prev;
+
+        Node1(int data) {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    public void insertAtEnd(int value) {
+        Node1 newNode = new Node1(value);
+
+        if (head == null) {
+            head = tail = newNode;
+            head.next = newNode;
+            head.prev = head;
+            return;
+        }
+
+        tail.next = newNode;
+        newNode.prev = tail;
+
+        newNode.next = head;
+        head.prev = newNode;
+
+        tail = newNode;
+    }
+
+    public void displayForward() {
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+
+        Node1 temp = head;
+        do {
+            System.out.print(temp.data + " ⇄ ");
+            temp = temp.next;
+        } while (temp != head);
+        System.out.println("(Back to head)");
+    }
+
+    public static void main(String[] args) {
+        CircularDoublyLinkedList cdll = new CircularDoublyLinkedList();
+
+        cdll.insertAtEnd(10);
+        cdll.insertAtEnd(20);
+        cdll.insertAtEnd(30);
+        cdll.insertAtEnd(40);
+        cdll.insertAtEnd(50);
+
+        cdll.displayForward();
+    }
+}
+
+
+/*
+
+Insert at End (Tail)
+🔹 Logic:
+If list is empty → set head and tail to newNode and connect circular links.
+
+Else → update tail.next = newNode, newNode.prev = tail, and update tail = newNode.
+
+Finally → make it circular:
+
+tail.next = head
+
+head.prev = tail
+
+ */
