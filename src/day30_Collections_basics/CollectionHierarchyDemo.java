c51fabe141b6b0a1ea36a4af0c3de416b0453f26
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-07-29 14:34:50 +0530

    Day 30, Collection Hierarchy Demo program

---
+++ src/day30_Collections_basics/CollectionHierarchyDemo.java
@@ -0,0 +1,26 @@
+package day30_Collections_basics;
+
+import java.util.*;
+
+public class CollectionHierarchyDemo {
+    public static void main(String[] args) {
+
+        List<String> list = new ArrayList<>();
+
+        Set<Integer> set = new HashSet<>();
+
+        Queue<String> queue = new LinkedList<>();
+
+        Map<Integer, String> map = new HashMap<>();
+
+        list.add("Munna");
+        set.add(100);
+        queue.add("Spring");
+        map.put(1, "Java");
+
+        System.out.println(list);  // [Munna]
+        System.out.println(set);   // [100]
+        System.out.println(queue); // [Spring]
+        System.out.println(map);   // {1=java}
+    }
+}
