a2dca5212d30557ad3dc3e2b6f6f6dd9ac0bdf3c
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-07-29 13:37:53 +0530

    Day 30, Collections basic demo program

---
+++ src/day30_Collections_basics/CollectionDemo.java
@@ -0,0 +1,17 @@
+package day30_Collections_basics;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class CollectionDemo {
+    public static void main(String[] args) {
+        Collection<String> fruits = new ArrayList<>();
+
+        fruits.add("Mango");
+        fruits.add("Banana");
+        fruits.add("Apple");
+        fruits.add("Chery");
+
+        System.out.println(fruits);   // [Mango, Banana, Apple, Chery]
+    }
+}
