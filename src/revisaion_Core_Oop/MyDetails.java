05936f814d3d28317c0924fe7832a9f9943ec764
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-08-09 13:01:33 +0530

    Java learning, Saturday revision java basic example

---
+++ src/revisaion_Core_Oop/MyDetails.java
@@ -0,0 +1,22 @@
+package revisaion_Core_Oop;
+
+public class MyDetails {
+    public static void main(String[] args) {
+
+        // Personal Details
+        String name = "Munna";
+        int graduationYear = 2022;
+        String city = "Kolkata";
+
+        // Printing Details
+        System.out.println("Name: " + name);
+        System.out.println("Graduation Year: " + graduationYear);
+        System.out.println("City: "+ city);
+    }
+}
+
+/*
+Name: Munna
+Graduation Year: 2022
+City: Kolkata
+ */
