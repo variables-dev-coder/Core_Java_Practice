228c41c13a9454fc2ba0efdfc00d2ace4f13a2f6
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-08-09 13:08:35 +0530

    Java Learning  Saturday Revision basic example

---
+++ src/revisaion_Core_Oop/DataTypeExample.java
@@ -0,0 +1,35 @@
+package revisaion_Core_Oop;
+
+public class DataTypeExample {
+    public static void main(String[] args) {
+        // Primitive types
+        byte age = 25;
+        int salary = 50000;
+        float height = 5.9f;
+        double pi = 3.141592653;
+        char grade = 'A';
+        boolean isJavaFun = true;
+
+        // Non-primitive
+        String name = "Munna";
+
+        // Printing
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Height: " + height);
+        System.out.println("PI value: " + pi);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Java fun? " + isJavaFun);
+    }
+}
+
+
+/*
+Interview Questions
+Difference between primitive and non-primitive data types?
+Why do we need L at the end of long values and f for floats?
+Can char store a number? (Yes, stores Unicode value)
+What is the default value of a boolean variable? (false)
+
+ */
