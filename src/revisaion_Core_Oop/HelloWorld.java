9f36ae9610d329381c50e1fc1547fe4c78e1a5d0
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-08-09 12:54:20 +0530

    Java Revision Basic example

---
+++ src/revisaion_Core_Oop/HelloWorld.java
@@ -0,0 +1,30 @@
+package revisaion_Core_Oop;
+
+public class HelloWorld {
+
+    /*
+    1️⃣ Java Introduction, JVM, JDK, JRE
+    Concept
+    JDK (Java Development Kit) → Contains tools for developing Java apps (compiler javac, debugger, libraries, JVM)
+    JRE (Java Runtime Environment) → Runs Java programs (JVM + libraries)
+    JVM (Java Virtual Machine) → Converts Java bytecode into machine code for your OS
+    Java is platform-independent because code is compiled into bytecode, which the JVM executes on any OS.
+
+     */
+
+    /*
+
+    Interview Questions
+    What is the difference between JDK, JRE, and JVM?
+    Why is Java platform-independent?
+    What is bytecode?
+    Is JVM platform-independent? (No – JVM is OS-specific, but bytecode is portable)
+
+     */
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World");
+    }
+}
