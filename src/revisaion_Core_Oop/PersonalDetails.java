02c8ffcbf9cfc61c79b4c5058538c6a71971a11e
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-08-09 13:16:17 +0530

    Java Learning, Saturday Revision Java Basic example

---
+++ src/revisaion_Core_Oop/PersonalDetails.java
@@ -0,0 +1,15 @@
+package revisaion_Core_Oop;
+
+public class PersonalDetails {
+    public static void main(String[] args) {
+
+        // Personal Details
+        byte age = 25;
+        int salary = 34000;
+        float height = 6.0f;
+        String name = "Munna";
+
+        // Printing details
+        System.out.println(name + ", Age: " + age + ", Salary: " + salary + ", Height: " + height);
+    }
+}
