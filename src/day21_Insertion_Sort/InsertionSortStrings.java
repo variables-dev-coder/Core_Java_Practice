b8f9232555e6b3386007a4866c5c903e406b3631
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-08-11 17:01:31 +0530

    Insertion Sort String Example

--- src/day21_Insertion_Sort/InsertionSortStrings.java
+++ src/day21_Insertion_Sort/InsertionSortStrings.java
@@ -32,3 +32,34 @@ public class InsertionSortStrings {
         }
     }
 }
+
+/*
+How compareTo() Works
+"apple".compareTo("banana") → negative → apple comes before banana.
+
+"cherry".compareTo("apple") → positive → cherry comes after apple.
+
+"grape".compareTo("grape") → 0 → same word.
+ */
+
+/*
+Iteration 1 (i=1, key="apple")
+Compare "banana" with "apple" → "banana".compareTo("apple") > 0 → shift banana.
+
+Insert "apple" → {"apple", "banana", "cherry", "mango", "grape"}
+
+Iteration 2 (i=2, key="cherry")
+"banana".compareTo("cherry") < 0 → no shift → same order.
+
+Iteration 3 (i=3, key="mango")
+"cherry".compareTo("mango") < 0 → no shift.
+
+Iteration 4 (i=4, key="grape")
+Compare "mango" with "grape" → "mango".compareTo("grape") > 0 → shift mango.
+
+Compare "cherry" with "grape" → "cherry".compareTo("grape") < 0 → stop shifting.
+
+Insert "grape" → {"apple", "banana", "cherry", "grape", "mango"}
+
+Sorted Array: {apple, banana, cherry, grape, mango}
+ */
