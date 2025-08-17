0fdc555dd99fbddec344f95ff754bc0ba6076896
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-08-17 14:03:58 +0530

    Day 32, Wrapper class erxample

---
+++ src/revision_3/WrapperExample10.java
@@ -0,0 +1,34 @@
+package revision_3;
+
+public class WrapperExample10 {
+    public static void main(String[] args) {
+        System.out.println("Integer MAX_VALUE: " + Integer.MAX_VALUE);
+        System.out.println("Integer MIN_VALUE: " + Integer.MIN_VALUE);
+        System.out.println("Integer SIZE (bits): " + Integer.SIZE);
+        System.out.println("Integer BYTES: " + Integer.BYTES);
+
+        System.out.println("Double MAX_VALUE: " + Double.MAX_VALUE);
+        System.out.println("Double MIN_VALUE: " + Double.MIN_VALUE);
+        System.out.println("Double SIZE (bits): " + Double.SIZE);
+        System.out.println("Double BYTES: " + Double.BYTES);
+
+        System.out.println("Character MAX_VALUE: " + (int) Character.MAX_VALUE);
+        System.out.println("Character MIN_VALUE: " + (int) Character.MIN_VALUE);
+        System.out.println("Character SIZE (bits): " + Character.SIZE);
+    }
+}
+
+/*
+Integer MAX_VALUE: 2147483647
+Integer MIN_VALUE: -2147483648
+Integer SIZE (bits): 32
+Integer BYTES: 4
+Double MAX_VALUE: 1.7976931348623157E308
+Double MIN_VALUE: 4.9E-324
+Double SIZE (bits): 64
+Double BYTES: 8
+Character MAX_VALUE: 65535
+Character MIN_VALUE: 0
+Character SIZE (bits): 16
+
+ */
