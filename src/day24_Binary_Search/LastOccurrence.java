086be8821e6fc1a584719bfe22a7e3fdbd717edc
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-08-18 13:01:08 +0530

    Java Binaray search last occurrence example

---
+++ src/day24_Binary_Search/LastOccurrence.java
@@ -0,0 +1,31 @@
+package day24_Binary_Search;
+
+public class LastOccurrence {
+    public static int lastOccurrence(int[] arr, int target) {
+        int low = 0;
+        int high = arr.length - 1;
+        int result = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == target) {
+                result = mid;
+                low = mid + 1;
+            } else if (arr[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 2, 3, 4, 5};
+        int target = 2;
+
+        int index = lastOccurrence(arr, target);
+        System.out.println("Last Occurrence of " + target + " = " + index);
+    }
+}
