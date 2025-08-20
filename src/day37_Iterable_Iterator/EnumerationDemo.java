85f228252da0532ac1d4fd186961668fc26f9a71
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-08-20 12:57:16 +0530

    4. What is the difference between Enumeration and Iterator?

--- src/day37_Iterable_Iterator/EnumerationDemo.java
+++ src/day37_Iterable_Iterator/EnumerationDemo.java
@@ -1,4 +1,33 @@
 package day37_Iterable_Iterator;
 
+/*
+4. What is the difference between Enumeration and Iterator?
+
+Answer:
+Enumeration (legacy, used with old classes like Vector, Hashtable) → has hasMoreElements() and nextElement().
+Iterator (modern, for all collections) → has hasNext(), next(), remove().
+
+Example:
+ */
+
+import java.util.Enumeration;
+import java.util.Vector;
+
 public class EnumerationDemo {
+    public static void main(String[] args) {
+        Vector<String> v = new Vector<>();
+        v.add("One");
+        v.add("Two");
+        v.add("Three");
+
+        Enumeration<String> e = v.elements();
+
+        while(e.hasMoreElements()) {
+            System.out.println(e.nextElement());
+        }
+    }
 }
+
+//One
+//Two
+//Three
