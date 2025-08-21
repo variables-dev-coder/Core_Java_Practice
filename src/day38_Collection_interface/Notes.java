fe70607a28d75035f1f0de3a557ccf129ff22957
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-08-21 11:22:52 +0530

    Java Day 38, Collection Interface

---
+++ src/day38_Collection_interface/Notes.java
@@ -0,0 +1,38 @@
+package day38_Collection_interface;
+
+public class Notes {
+    public static void main(String[] args) {
+
+        /*
+
+Collection Interface in Java
+    Root interface of the Collection Framework hierarchy (but not of Map hierarchy).
+    Declares the common operations for all collections (like add, remove, size, etc).
+
+Implemented by:
+    List (ArrayList, LinkedList, Vector, Stack)
+    Set (HashSet, LinkedHashSet, TreeSet)
+    Queue (PriorityQueue, ArrayDeque, LinkedList Queue)
+
+Methods in Collection Interface
+
+| Method                                      | Description                               |
+| ------------------------------------------- | ----------------------------------------- |
+| `boolean add(E e)`                          | Adds an element                           |
+| `boolean remove(Object o)`                  | Removes element                           |
+| `int size()`                                | Returns number of elements                |
+| `void clear()`                              | Removes all elements                      |
+| `boolean contains(Object o)`                | Checks if element exists                  |
+| `boolean isEmpty()`                         | Checks if collection is empty             |
+| `Iterator<E> iterator()`                    | Returns an iterator                       |
+| `boolean addAll(Collection<? extends E> c)` | Adds all elements from another collection |
+| `boolean removeAll(Collection<?> c)`        | Removes all elements that match           |
+| `boolean retainAll(Collection<?> c)`        | Keeps only matching elements              |
+| `Object[] toArray()`                        | Converts collection to array              |
+
+
+
+
+         */
+    }
+}
