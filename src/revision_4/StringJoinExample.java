c030427636a18e43311e920fb617f0aa6bea8f0e
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-08-23 13:30:29 +0530

    String Join Example

---
+++ src/revision_4/StringJoinExample.java
@@ -0,0 +1,19 @@
+package revision_4;
+
+public class StringJoinExample {
+    public static void main(String[] args) {
+        String result = String.join(", ", "Java", "Python", "C++", "Go");
+        System.out.println(result);
+    }
+}
+
+// Java, Python, C++, Go
+
+/*
+Topic 12: String Joiner (StringJoiner & String.join())
+
+Java provides StringJoiner (introduced in Java 8) and String.join()
+to easily concatenate strings with delimiters, prefixes, and suffixes.
+
+
+ */
---
+++ src/revision_4/StringJoinerExample.java
@@ -0,0 +1,15 @@
+package revision_4;
+
+import java.util.StringJoiner;
+
+public class StringJoinerExample {
+
+    public static void main(String[] args) {
+        StringJoiner joiner = new StringJoiner(", ", "[", "]");
+        joiner.add("Apple");
+        joiner.add("Banana");
+        joiner.add("Mango");
+
+        System.out.println(joiner);
+    }
+}
