42c7ba1c581e6311105a01994d7591a1f6f1c4f7
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-08-27 14:56:15 +0530

    Java Day 42, Collection Stack example

---
+++ src/day42_Collection_Stack/Notes.java
@@ -0,0 +1,36 @@
+package day42_Collection_Stack;
+
+public class Notes {
+    public static void main(String[] args) {
+
+        /*
+
+Stack in Java (Collection Framework)
+1. Definition
+    A Stack is a linear data structure that follows the LIFO (Last In, First Out) principle.
+    In Java, it is implemented as a class Stack<E> which extends Vector<E>.
+    This means Stack inherits all features of Vector, but also adds special methods for stack operations.
+
+2. Hierarchy
+Object
+  ↳ Collection
+      ↳ List
+          ↳ Vector
+              ↳ Stack
+
+
+3. Common Methods of Stack
+
+| Method             | Description                                                       |
+| ------------------ | ----------------------------------------------------------------- |
+| `push(E item)`     | Inserts an item onto the top of the stack                         |
+| `pop()`            | Removes and returns the top item of the stack                     |
+| `peek()`           | Returns the top item without removing it                          |
+| `empty()`          | Returns true if the stack is empty                                |
+| `search(Object o)` | Returns position of object from top (1-based), or -1 if not found |
+
+
+
+         */
+    }
+}
