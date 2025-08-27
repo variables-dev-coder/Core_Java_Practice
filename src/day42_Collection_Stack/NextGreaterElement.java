fb3e4e06f351dcbb9d524de91fe43c89740ec40f
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-08-27 15:43:06 +0530

    Java Day 42 Collection Stack problem

---
+++ src/day42_Collection_Stack/NextGreaterElement.java
@@ -0,0 +1,38 @@
+package day42_Collection_Stack;
+
+/*
+Problem 2: Next Greater Element
+
+For each element, find the next greater element on the right.
+
+Input: [4, 5, 2, 25]
+Output: [5, 25, 25, -1]
+
+Logic: Traverse from right → left, use stack to store greater elements.
+ */
+
+
+import java.util.Stack;
+
+public class NextGreaterElement {
+    public static int[] nextGreater(int[] arr) {
+        int n = arr.length;
+        int[] result = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && stack.peek() <= arr[i]) {
+                stack.pop();
+            }
+            result[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(arr[i]);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 2, 25};
+        System.out.println(Arrays.toString(nextGreater(arr)));
+        // Output: [5, 25, 25, -1]
+    }
+}
