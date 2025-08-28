af3ac4c9ad122e78e878d120903b9bc3c2376f04
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-08-28 19:21:36 +0530

    Java Day 43, Collection Priority Queue example

---
+++ src/day43_Collection_PriorityQueue/KthLargestElement.java
@@ -0,0 +1,22 @@
+package day43_Collection_PriorityQueue;
+
+import java.util.PriorityQueue;
+
+public class KthLargestElement {
+    public static int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        for (int num : nums) {
+            minHeap.add(num);
+            if (minHeap.size() > k) {
+                minHeap.poll(); // remove smallest
+            }
+        }
+        return minHeap.peek(); // root = kth largest
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3,2,1,5,6,4};
+        System.out.println(findKthLargest(arr, 2)); // Output: 5
+    }
+}
