8d928d0a462a7e57e5f0e532e1f1fd5352c63d33
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-09-04 19:03:07 +0530

    Java Day 49, Multithreading example

---
+++ src/day49_Multithreading/BasicOfMultithreading/Q4.java
@@ -0,0 +1,31 @@
+package day49_Multithreading.BasicOfMultithreading;
+
+
+
+class Worker extends Thread {
+    public void run() {
+        for (int i = 1; i <= 3; i++) {
+            System.out.println("Worker Running Step " + i);
+        }
+    }
+}
+
+public class Q4 {
+    public static void main(String[] args) throws InterruptedException {
+        Worker worker = new Worker();
+        worker.start();
+
+        worker.join();  // main waits until worker completes
+
+        System.out.println("Main thread finished after worker.");
+
+    }
+}
+
+/*
+Worker Running Step 1
+Worker Running Step 2
+Worker Running Step 3
+Main thread finished after worker.
+
+ */
