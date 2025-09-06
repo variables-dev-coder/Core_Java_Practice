aea7361e14487ea155e487fe9629a8b4fceb85ed
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-09-06 12:37:26 +0530

    Java DDay 51, Collection problem

---
+++ src/revision_8/AnagramCheck.java
@@ -0,0 +1,21 @@
+package revision_8;
+
+import java.util.Arrays;
+
+public class AnagramCheck {
+    public static boolean isAnagram(String s1, String s2) {
+        if (s1.length() != s2.length()) return false;
+
+        char[] arr1 = s1.toCharArray();
+        char[] arr2 = s2.toCharArray();
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        return Arrays.equals(arr1, arr2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isAnagram("listen", "silent")); // true
+        System.out.println(isAnagram("hello", "world"));   // false
+    }
+}
