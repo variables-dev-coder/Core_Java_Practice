add6cd810c779a4fb28bc6bd78dbf5d1b581945f
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-09-13 21:39:35 +0530

    Java Learning Day 57, problem 1

---
+++ src/revision_10/Example7.java
@@ -0,0 +1,13 @@
+package revision_10;
+
+public class Example7 {
+    public static void main(String[] args) {
+        try {
+            int result = 10 / 0;
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero");
+        }
+    }
+}
+
+//Cannot divide by zero
