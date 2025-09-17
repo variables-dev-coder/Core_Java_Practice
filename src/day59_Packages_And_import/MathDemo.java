0d4ad102e53a4cf5b750632b4cc3917218d305e9
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-09-17 15:01:13 +0530

    Java Day 59, Packages, Static import example

---
+++ src/day59_Packages_And_import/MathDemo.java
@@ -0,0 +1,16 @@
+package day59_Packages_And_import;
+
+import static java.lang.Math.*;
+
+public class MathDemo {
+    public static void main(String[] args) {
+        double r = 7;
+        System.out.println("Circle Area: " + (PI * pow(r, 2)));
+        System.out.println("Sqrt(25): " + sqrt(25));
+        System.out.println("Max: " + max(10, 20));
+    }
+}
+
+//Circle Area: 153.93804002589985
+//Sqrt(25): 5.0
+//Max: 20
