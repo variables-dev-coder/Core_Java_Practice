5ec413b3c4395426618ab847ff60eb8c72c7c0ce
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-09-17 14:13:34 +0530

    Java Day 59, Packages, Static import

---
+++ src/day59_Packages_And_import/MathExample.java
@@ -0,0 +1,15 @@
+package day59_Packages_And_import;
+
+import static java.lang.Math.*;   // Import all static methods of Math
+
+public class MathExample {
+    public static void main(String[] args) {
+        System.out.println(sqrt(16));        // Instead of Math.sqrt()
+        System.out.println(max(10, 20));
+        System.out.println(pow(2, 5));
+    }
+}
+
+//4.0
+//20
+//32.0
