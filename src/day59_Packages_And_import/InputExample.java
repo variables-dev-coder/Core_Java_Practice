073f040cc5fd86189fd87b5755f3b7d3cf906d20
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-09-17 14:06:53 +0530

    Java, Day 59, import Specific class

---
+++ src/day59_Packages_And_import/InputExample.java
@@ -0,0 +1,12 @@
+package day59_Packages_And_import;
+
+import java.util.Scanner;
+
+public class InputExample {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter name: ");
+        String name = sc.nextLine();
+        System.out.println("Hello, I love You " + name);
+    }
+}
