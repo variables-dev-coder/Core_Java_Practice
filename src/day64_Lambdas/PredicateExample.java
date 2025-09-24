17759e611784941181be5e44d5e126d8cef5e346
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-09-24 21:55:23 +0530

    Java Day 64, Predicate (Check Even Number) example

---
+++ src/day64_Lambdas/PredicateExample.java
@@ -0,0 +1,13 @@
+package day64_Lambdas;
+
+// Predicate (Check Even Number)
+
+import java.util.function.Predicate;
+
+public class PredicateExample {
+    public static void main(String[] args) {
+        Predicate<Integer> isEven = n -> n % 2 == 0;
+        System.out.println(isEven.test(10)); // true
+        System.out.println(isEven.test(11)); // false
+    }
+}
