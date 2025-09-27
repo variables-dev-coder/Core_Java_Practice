699932621ffb53d7fc9c9627abdc369fbefc3009
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-09-27 10:28:59 +0530

    Java Day 66, The main Method & Basic Program Structure

---
+++ src/revision_11/MainApp.java
@@ -0,0 +1,21 @@
+package revision_11;
+
+
+/*
+I. Foundational Syntax & Structure
+    1. The main Method & Basic Program Structure
+        Every standalone Java app starts execution from public static void main(String[] args).
+
+Example:
+ */
+
+public class MainApp {
+    public static void main(String[] args) {
+        System.out.println("Hello, Darling");
+    }
+}
+
+/*
+Hello, Darling
+
+ */
