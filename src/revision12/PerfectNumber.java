dcd65b4b89c6d4d3266e6a02b1516eb9d0f2ecc6
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-11-11 23:24:56 +0530

    Perfect Number

---
+++ src/revision12/PerfectNumber.java
@@ -0,0 +1,15 @@
+package revision12;
+
+public class PerfectNumber {
+    public static void main(String[] args) {
+        int num = 28, sum = 0;
+
+        for (int i = 1; i <= num / 2; i++) {
+            if (num % i == 0) sum += i;
+        }
+
+        System.out.println(num + " is perfect: " + (sum == num));
+    }
+}
+
+// 28 is perfect: true
