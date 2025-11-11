c79dd0000403680293d4429d56e394022467c541
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-11-11 23:19:19 +0530

    Linear Search

---
+++ src/revision12/LinearSearch.java
@@ -0,0 +1,17 @@
+package revision12;
+
+public class LinearSearch {
+    public static int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) return i;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        System.out.println("Index of 30: " + linearSearch(arr, 30));
+    }
+}
+
+// Index of 30: 2
