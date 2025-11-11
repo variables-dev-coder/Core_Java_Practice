200e248cbef70a7180fad6d4db0d6b78527e3fed
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-11-11 23:14:48 +0530

    Reversed Number

---
+++ src/revision12/ReverseNumber.java
@@ -0,0 +1,17 @@
+package revision12;
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+        int num = 12345;
+        int reversed = 0;
+
+        while (num != 0) {
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num /= 10;
+        }
+        System.out.println("Reversed: " + reversed);
+    }
+}
+
+// Reversed: 54321
