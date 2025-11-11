fa68244749e060f4c2f03736faabad2337ce9823
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-11-11 23:30:08 +0530

    Matrix multiplacation

---
+++ src/revision12/MatrixMultiplication.java
@@ -0,0 +1,27 @@
+package revision12;
+
+public class MatrixMultiplication {
+    public static void main(String[] args) {
+        int[][] a = {{1, 2, 3}, {4, 5, 6}};
+        int[][] b = {{7, 8}, {9, 10}, {11, 12}};
+        int[][] result = new int[2][2];
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                for (int k = 0; k < 3; k++) {
+                    result[i][j] += a[i][k] * b[k][j];
+                }
+            }
+        }
+
+        for (int[] row : result) {
+            for (int val : row) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+//58 64
+//139 154
