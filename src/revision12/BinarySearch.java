dd8a023948111d302202a3ea9b9b5d8a4a71e69e
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-11-11 22:10:58 +0530

    Binary search

---
+++ src/revision12/BinarySearch.java
@@ -0,0 +1,24 @@
+package revision12;
+
+public class BinarySearch {
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) return mid;
+            else if (arr[mid] < target) left = mid + 1;
+            else right = mid - 1;
+        }
+        return - 1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 5, 7, 9, 11};
+        System.out.println("Index of 7: " + binarySearch(arr, 7));
+    }
+}
+
+// Index of 7: 3
