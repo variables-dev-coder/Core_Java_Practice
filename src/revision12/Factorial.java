e739c8351ddace66680f3497224681a9ed7dcca7
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-11-11 22:01:46 +0530

    Factorial calculating

---
+++ src/revision12/Factorial.java
@@ -0,0 +1,15 @@
+package revision12;
+
+public class Factorial {
+    public static int factorial(int n) {
+        if (n == 0) return 1;
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Factorial of 5: " + factorial(5));
+    }
+}
+
+
+// Factorial of 5: 120
