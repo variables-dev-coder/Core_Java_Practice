a3de70421cdc622ab20b300c189bb07eaffcc4ba
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-11-11 23:37:03 +0530

    Check vowel and Consonant

---
+++ src/revision12/VowelConsonant.java
@@ -0,0 +1,16 @@
+package revision12;
+
+public class VowelConsonant {
+    public static void main(String[] args) {
+        char ch = 'e';
+
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+                ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+            System.out.println(ch + " is vowel");
+        } else {
+            System.out.println(ch + " is consonant");
+        }
+    }
+}
+
+// e is vowel
