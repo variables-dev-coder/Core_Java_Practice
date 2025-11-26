cd02826f5e64632987d5801691d26666afb7a008
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-11-26 15:21:47 +0530

    Is Palindrome

---
+++ src/revision_11/Palindrome.java
@@ -0,0 +1,20 @@
+package revision_11;
+
+public class Palindrome {
+    public static boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("level"));    // true
+        System.out.println(isPalindrome("hello"));    // false
+    }
+}
