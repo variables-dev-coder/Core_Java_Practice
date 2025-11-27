5b261b6baa3b79dc761f7281e2631b65fde0b3be
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-11-27 15:13:00 +0530

    Java Learning Day 75, Modern I/O Day 7, read file example

---
+++ src/day75_Modern_I_O_Reading_Writing_Files/Ex1_ReadFile.java
@@ -0,0 +1,16 @@
+package day75_Modern_I_O_Reading_Writing_Files;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Ex1_ReadFile {
+    public static void main(String[] args) throws Exception {
+        Path path = Paths.get("D:/SKILL/JAVA/Core_Java_Practice/src/day75_Modern_I_O_Reading_Writing_Files/example1.txt");
+
+        List<String> lines = Files.readAllLines(path);
+
+        lines.forEach(System.out::println);
+    }
+}
