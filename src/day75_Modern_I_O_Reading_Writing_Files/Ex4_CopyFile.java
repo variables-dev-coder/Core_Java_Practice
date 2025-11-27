7e396dd385d3b0e214036c106d7294ab9bc05c93
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-11-27 15:36:32 +0530

    Java Learning Day 75, Modern I/O Day 7, Copy File example

---
+++ src/day75_Modern_I_O_Reading_Writing_Files/Ex4_CopyFile.java
@@ -0,0 +1,15 @@
+package day75_Modern_I_O_Reading_Writing_Files;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+public class Ex4_CopyFile {
+    public static void main(String[] args) throws Exception {
+        Path src = Paths.get("D:/SKILL/JAVA/Core_Java_Practice/src/day75_Modern_I_O_Reading_Writing_Files/Example4.txt");
+        Path dest = Paths.get("D:/SKILL/JAVA/Core_Java_Practice/src/day75_Modern_I_O_Reading_Writing_Files/Example4_Copy.txt");
+
+        Files.copy(src, dest, StandardCopyOption.REPLACE_EXISTING);
+    }
+}
