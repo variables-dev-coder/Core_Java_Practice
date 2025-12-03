721cbf5848280babfd17838ed7e79e6030a1a2a1
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-12-03 23:44:55 +0530

    Is Subsequence

---
+++ src/revision15/IsSubsequence.java
@@ -0,0 +1,22 @@
+package revision15;
+
+public class IsSubsequence {
+    public static boolean isSubsequence(String s, String t) {
+        int i = 0, j = 0;
+
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+            j++;
+        }
+
+        return i == s.length();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isSubsequence("abc", "ahbgdc")); // true
+        System.out.println(isSubsequence("axc", "ahbgdc")); // false
+        System.out.println(isSubsequence("", "ahbgdc"));    // true
+    }
+}
